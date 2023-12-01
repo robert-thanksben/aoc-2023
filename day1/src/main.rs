44fc3336135ceb721664933196aebb9980674d22
Robert Engmann <robert@thanksben.com>
2023-12-01 23:55:27 +0100

    Cleanup for day1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,13 +1,8 @@
 use std::fs;
 use regex::Regex;
 use aho_corasick::AhoCorasick;
-// Part 1:
-// On each line, the calibration value can be found by combining 
-// the first digit and the last digit (in that order) 
-// to form a single two-digit number.
-// What is the sum of all of the calibration values?
-//
-fn part_1(input: &str) {
+
+fn part_1(input: &str) -> u32 {
     let re = Regex::new(r"\d").unwrap();
     let mut calibration_values: Vec<u32> = Vec::new();
 
@@ -20,55 +15,71 @@ fn part_1(input: &str) {
         }
     }
 
-    let sum: u32 = calibration_values.iter().sum();
-    println!("Part 1: SUM(Calibration Values): {}", sum);
+    calibration_values.iter().sum()
 }
 
-// Part 2:
-// Your calculation isn't quite right. It looks like some of the 
-// digits are actually spelled out with letters: one, two, three, 
-// four, five, six, seven, eight, and nine also count as valid "digits".
-//
-fn parse_chunk(chunk: &str) -> Option<&str> {
-    match chunk.to_lowercase().as_str() {
-        "one" => Some("1"),
-        "two" => Some("2"),
-        "three" => Some("3"),
-        "four" => Some("4"),
-        "five" => Some("5"),
-        "six" => Some("6"),
-        "seven" => Some("7"),
-        "eight" => Some("8"),
-        "nine" => Some("9"),
-        _ => Some(chunk),
-    }
-}
+fn part_2(input: &str) -> u32 {
+    let patterns = &["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", 
+                     "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-fn part_2(input: &str) {
-    let re = &["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    let ac = AhoCorasick::new(re).unwrap();
-    let mut calibration_values: Vec<usize> = Vec::new();
+    let ac = AhoCorasick::new(patterns).unwrap();
+    let mut calibration_values: Vec<u32> = Vec::new();
     
     for line in input.split("\n") {
-        let mut digits: Vec<&str> = Vec::new(); 
+        let mut digits: Vec<&str> = Vec::new();
+
         for m in ac.find_overlapping_iter(line) {
-            let digit: &str = re.get(m.pattern().as_usize()).unwrap();
-            digits.push(parse_chunk(digit).unwrap());
+            let matched_digit: &str = patterns.get(m.pattern().as_usize()).unwrap();
+
+            let converted_digit = match matched_digit {
+                "one" => Some("1"),
+                "two" => Some("2"),
+                "three" => Some("3"),
+                "four" => Some("4"),
+                "five" => Some("5"),
+                "six" => Some("6"),
+                "seven" => Some("7"),
+                "eight" => Some("8"),
+                "nine" => Some("9"),
+                _ => Some(matched_digit),
+            }.unwrap();
+
+            digits.push(converted_digit);
         }
 
         if !digits.is_empty() {
             let calibration_value = format!("{}{}", digits.first().unwrap(), digits.last().unwrap());
-            calibration_values.push(calibration_value.parse::<usize>().unwrap());
+            calibration_values.push(calibration_value.parse::<u32>().unwrap());
         }
     }
 
-    let sum: usize = calibration_values.iter().sum();
-    println!("Part 2: SUM(Calibration Values): {}", sum);
+    calibration_values.iter().sum()
 }
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Can't read file");
     
-    part_1(&input);
-    part_2(&input);
+    let result_1 = part_1(&input);
+    println!("Part 1: {}", result_1);
+
+    let result_2 = part_2(&input);
+    println!("Part 2: {}", result_2);
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+        let input = fs::read_to_string("input.txt").expect("Can't read file");
+        assert_eq!(part_1(&input), 54634);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let input = fs::read_to_string("input.txt").expect("Can't read file");
+        assert_eq!(part_2(&input), 53855);
+    }
 }
