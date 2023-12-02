8f4ec71ec47cdd6307d27d379f04f995303a69e9
Robert Engmann <robert@thanksben.com>
2023-12-02 23:21:49 +0100

    Day2 refactoring

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -2,27 +2,38 @@ use std::fs;
 use lazy_static::lazy_static;
 use regex::Regex;
 use std::collections::HashMap;
+use strum_macros::EnumString;
+use std::str::FromStr;
+
+#[derive(Eq, Hash, PartialEq, EnumString)]
+#[strum(serialize_all = "snake_case")]
+enum Color {
+    Red,
+    Green,
+    Blue
+}
 
 lazy_static! {
     static ref INPUT: String = fs::read_to_string("input.txt").expect("Can't read input file");
+    static ref CUBE_RX: HashMap<Color, u32> = HashMap::from([(Color::Red, 12), 
+                                                                       (Color::Green, 13), 
+                                                                       (Color::Blue, 14)]);
 }
 
 fn part_1(input: &str) -> u32 {
-    let max_cube_counts: HashMap<&str, u32> = HashMap::from([("red", 12), ("green", 13), ("blue", 14)]);
     let game_re = Regex::new(r"Game (\d+): (.*)").unwrap();
     let color_re = Regex::new(r"(\d+) (red|green|blue)").unwrap();
-
     let mut game_count = 0;
 
     for cap in game_re.captures_iter(input) {
         let game_number: u32 = cap[1].parse().unwrap();
         let content = &cap[2];
-
-        let mut color_counts: HashMap<String, u32> = HashMap::new();
+        let mut color_counts: HashMap<Color, u32> = HashMap::new();
+        let mut update = true;
 
         for color_cap in color_re.captures_iter(content) {
             let count: u32 = color_cap[1].parse().unwrap();
-            let color = color_cap[2].to_string();
+            let color = Color::from_str(&color_cap[2]).unwrap();
             let entry = color_counts.entry(color).or_insert(count);
 
             if *entry < count {
@@ -30,14 +41,11 @@ fn part_1(input: &str) -> u32 {
             }
         }
         
-        let mut update = true;
-
         for (cc_key, cc_value) in color_counts.iter() {
-            if max_cube_counts.get(cc_key as &str).unwrap() < cc_value {
+            if CUBE_RX.get(&cc_key).unwrap() < cc_value {
                 update = false;
                 break;
             }
-
         }
 
         if update {
@@ -51,17 +59,15 @@ fn part_1(input: &str) -> u32 {
 fn part_2(input: &str) -> u32 {
     let game_re = Regex::new(r"Game (\d+): (.*)").unwrap();
     let color_re = Regex::new(r"(\d+) (red|green|blue)").unwrap();
-
     let mut sum_power_set: u32 = 0;
 
     for cap in game_re.captures_iter(input) {
         let content = &cap[2];
-
-        let mut color_counts: HashMap<String, u32> = HashMap::new();
+        let mut color_counts: HashMap<Color, u32> = HashMap::new();
 
         for color_cap in color_re.captures_iter(content) {
             let count: u32 = color_cap[1].parse().unwrap();
-            let color = color_cap[2].to_string();
+            let color = Color::from_str(&color_cap[2]).unwrap();
             let entry = color_counts.entry(color).or_insert(count);
 
             if *entry < count {
