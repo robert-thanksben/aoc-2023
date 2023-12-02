82dabe907b5efc45bb55bc3603cd0c54e20b4934
Robert Engmann <robert@thanksben.com>
2023-12-02 04:46:14 +0100

    Final refactor  Changes to be committed:

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,7 +1,11 @@
-use std::fs;
-use regex::Regex;
 use aho_corasick::AhoCorasick;
+use lazy_static::lazy_static;
+use regex::Regex;
+use std::fs;
 
+lazy_static! {
+    static ref INPUT: String = fs::read_to_string("input.txt").expect("Can't read file");
+}
 
 fn push_value(calibration_values: &mut Vec<u32>, digits: &Vec<&str>) {
     if !digits.is_empty() {
@@ -26,33 +30,30 @@ fn part_1(input: &str) -> u32 {
 }
 
 fn part_2(input: &str) -> u32 {
-    let patterns = &["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", 
-                     "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    let patterns = &[
+        "1", "2", "3", "4", "5", "6", "7", "8", "9", 
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
 
     let ac = AhoCorasick::new(patterns).unwrap();
     let mut calibration_values: Vec<u32> = Vec::new();
-    
+
     for line in input.split("\n") {
-        let mut digits: Vec<&str> = Vec::new();
-
-        for matched in ac.find_overlapping_iter(line) {
-            let matched_digit: &str = patterns.get(matched.pattern().as_usize()).unwrap();
-
-            let converted_digit = match matched_digit {
-                "one" => Some("1"),
-                "two" => Some("2"),
-                "three" => Some("3"),
-                "four" => Some("4"),
-                "five" => Some("5"),
-                "six" => Some("6"),
-                "seven" => Some("7"),
-                "eight" => Some("8"),
-                "nine" => Some("9"),
-                _ => Some(matched_digit),
-            }.unwrap();
-
-            digits.push(converted_digit);
-        }
+        let digits: Vec<&str> = ac
+            .find_overlapping_iter(line)
+            .map(|mat| match patterns[mat.pattern()].as_ref() {
+                "one" => "1",
+                "two" => "2",
+                "three" => "3",
+                "four" => "4",
+                "five" => "5",
+                "six" => "6",
+                "seven" => "7",
+                "eight" => "8",
+                "nine" => "9",
+                digit => digit,
+            })
+            .collect();
 
         push_value(&mut calibration_values, &digits);
     }
@@ -61,24 +62,17 @@ fn part_2(input: &str) -> u32 {
 }
 
 fn main() {
-    let input = fs::read_to_string("input.txt").expect("Can't read file");
-    
-    let result_1 = part_1(&input);
-    println!("Part 1: {}", result_1);
-
-    let result_2 = part_2(&input);
-    println!("Part 2: {}", result_2);
+    println!("Part 1: {}", part_1(&INPUT));
+    println!("Part 2: {}", part_2(&INPUT));
 }
 
 #[test]
 fn test_part_1() {
-    let input = fs::read_to_string("input.txt").expect("Can't read file");
-    assert_eq!(part_1(&input), 54634);
+    assert_eq!(part_1(&INPUT), 54634);
 }
 
 #[test]
 fn test_part_2() {
-    let input = fs::read_to_string("input.txt").expect("Can't read file");
-    assert_eq!(part_2(&input), 53855);
+    assert_eq!(part_2(&INPUT), 53855);
 }
 
