68db2ee5e82eecba961af3e6a38f276584de585e
Robert Engmann <robert@thanksben.com>
2023-12-02 23:28:14 +0100

    More refactoring

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -15,22 +15,22 @@ enum Color {
 
 lazy_static! {
     static ref INPUT: String = fs::read_to_string("input.txt").expect("Can't read input file");
+    static ref GAME_RE: Regex = Regex::new(r"Game (\d+): (.*)").unwrap();
+    static ref COLOR_RE: Regex = Regex::new(r"(\d+) (red|green|blue)").unwrap();
     static ref CUBE_RX: HashMap<Color, u32> = HashMap::from([(Color::Red, 12), 
                                                              (Color::Green, 13), 
                                                              (Color::Blue, 14)]);
 }
 
 fn part_1(input: &str) -> u32 {
-    let game_re = Regex::new(r"Game (\d+): (.*)").unwrap();
-    let color_re = Regex::new(r"(\d+) (red|green|blue)").unwrap();
     let mut game_count = 0;
 
-    for cap in game_re.captures_iter(input) {
+    for cap in GAME_RE.captures_iter(input) {
         let game_number: u32 = cap[1].parse().unwrap();
         let mut color_counts: HashMap<Color, u32> = HashMap::new();
         let mut update = true;
 
-        for color_cap in color_re.captures_iter(&cap[2]) {
+        for color_cap in COLOR_RE.captures_iter(&cap[2]) {
             let count: u32 = color_cap[1].parse().unwrap();
             let color = Color::from_str(&color_cap[2]).unwrap();
             let entry = color_counts.entry(color).or_insert(count);
@@ -56,14 +56,12 @@ fn part_1(input: &str) -> u32 {
 }
 
 fn part_2(input: &str) -> u32 {
-    let game_re = Regex::new(r"Game (\d+): (.*)").unwrap();
-    let color_re = Regex::new(r"(\d+) (red|green|blue)").unwrap();
     let mut sum_power_set: u32 = 0;
 
-    for cap in game_re.captures_iter(input) {
+    for cap in GAME_RE.captures_iter(input) {
         let mut color_counts: HashMap<Color, u32> = HashMap::new();
 
-        for color_cap in color_re.captures_iter(&cap[2]) {
+        for color_cap in COLOR_RE.captures_iter(&cap[2]) {
             let count: u32 = color_cap[1].parse().unwrap();
             let color = Color::from_str(&color_cap[2]).unwrap();
             let entry = color_counts.entry(color).or_insert(count);
